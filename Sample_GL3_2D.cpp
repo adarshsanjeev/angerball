de69f92f3c534f89c7857478b249d2ecc3548290
Adarsh <adarshsanjeev@gmail.com>
2016-01-17 18:29:07 +0530

    Added key

--- Sample_GL3_2D.cpp
+++ Sample_GL3_2D.cpp
@@ -37,23 +37,23 @@ GLuint programID;
 
 class Character {
 public:
-  float x, y;
-  float angle;
-  float vel_x, vel_y;
-  float colour;
-  VAO sprite;
-  void fall() {
+	float x, y;
+	float angle;
+	float vel_x, vel_y;
+	float colour;
+	VAO *sprite;
+	void fall() {
 
-  }
+	}
 };
 
 Character Bird;
 
 class Wall {
 public:
-  float x, y;
-  float colour;
-  VAO *sprite;
+	float x, y;
+	float colour;
+	VAO *sprite;
 };
 
 Wall Floor;
@@ -168,24 +168,24 @@ struct VAO* create3DObject (GLenum primitive_mode, int numVertices, const GLfloa
     glBindBuffer (GL_ARRAY_BUFFER, vao->VertexBuffer); // Bind the VBO vertices 
     glBufferData (GL_ARRAY_BUFFER, 3*numVertices*sizeof(GLfloat), vertex_buffer_data, GL_STATIC_DRAW); // Copy the vertices into VBO
     glVertexAttribPointer(
-                          0,                  // attribute 0. Vertices
-                          3,                  // size (x,y,z)
-                          GL_FLOAT,           // type
-                          GL_FALSE,           // normalized?
-                          0,                  // stride
-                          (void*)0            // array buffer offset
-                          );
+		0,                  // attribute 0. Vertices
+		3,                  // size (x,y,z)
+		GL_FLOAT,           // type
+		GL_FALSE,           // normalized?
+		0,                  // stride
+		(void*)0            // array buffer offset
+		);
 
     glBindBuffer (GL_ARRAY_BUFFER, vao->ColorBuffer); // Bind the VBO colors 
     glBufferData (GL_ARRAY_BUFFER, 3*numVertices*sizeof(GLfloat), color_buffer_data, GL_STATIC_DRAW);  // Copy the vertex colors
     glVertexAttribPointer(
-                          1,                  // attribute 1. Color
-                          3,                  // size (r,g,b)
-                          GL_FLOAT,           // type
-                          GL_FALSE,           // normalized?
-                          0,                  // stride
-                          (void*)0            // array buffer offset
-                          );
+		1,                  // attribute 1. Color
+		3,                  // size (r,g,b)
+		GL_FLOAT,           // type
+		GL_FALSE,           // normalized?
+		0,                  // stride
+		(void*)0            // array buffer offset
+		);
 
     return vao;
 }
@@ -239,30 +239,30 @@ bool rectangle_rot_status = true;
 /* Prefered for Keyboard events */
 void keyboard (GLFWwindow* window, int key, int scancode, int action, int mods)
 {
-     // Function is called first on GLFW_PRESS.
+	// Function is called first on GLFW_PRESS.
 
     if (action == GLFW_RELEASE) {
         switch (key) {
-            case GLFW_KEY_C:
-                rectangle_rot_status = !rectangle_rot_status;
-                break;
-            case GLFW_KEY_P:
-                triangle_rot_status = !triangle_rot_status;
-                break;
-            case GLFW_KEY_X:
-                // do something ..
-                break;
-            default:
-                break;
+		case GLFW_KEY_C:
+			rectangle_rot_status = !rectangle_rot_status;
+			break;
+		case GLFW_KEY_P:
+			triangle_rot_status = !triangle_rot_status;
+			break;
+		case GLFW_KEY_X:
+			// do something ..
+			break;
+		default:
+			break;
         }
     }
     else if (action == GLFW_PRESS) {
         switch (key) {
-            case GLFW_KEY_ESCAPE:
-                quit(window);
-                break;
-            default:
-                break;
+		case GLFW_KEY_ESCAPE:
+			quit(window);
+			break;
+		default:
+			break;
         }
     }
 }
@@ -271,12 +271,15 @@ void keyboard (GLFWwindow* window, int key, int scancode, int action, int mods)
 void keyboardChar (GLFWwindow* window, unsigned int key)
 {
 	switch (key) {
-		case 'Q':
-		case 'q':
-            quit(window);
-            break;
-		default:
-			break;
+	case 'Q':
+	case 'q':
+		quit(window);
+		break;
+	case 'w':
+		Bird.y += 1;
+		break;
+	default:
+		break;
 	}
 }
 
@@ -284,17 +287,17 @@ void keyboardChar (GLFWwindow* window, unsigned int key)
 void mouseButton (GLFWwindow* window, int button, int action, int mods)
 {
     switch (button) {
-        case GLFW_MOUSE_BUTTON_LEFT:
-            if (action == GLFW_RELEASE)
-                triangle_rot_dir *= -1;
-            break;
-        case GLFW_MOUSE_BUTTON_RIGHT:
-            if (action == GLFW_RELEASE) {
-                rectangle_rot_dir *= -1;
-            }
-            break;
-        default:
-            break;
+	case GLFW_MOUSE_BUTTON_LEFT:
+		if (action == GLFW_RELEASE)
+			triangle_rot_dir *= -1;
+		break;
+	case GLFW_MOUSE_BUTTON_RIGHT:
+		if (action == GLFW_RELEASE) {
+			rectangle_rot_dir *= -1;
+		}
+		break;
+	default:
+		break;
     }
 }
 
@@ -305,7 +308,7 @@ void reshapeWindow (GLFWwindow* window, int width, int height)
 {
     int fbwidth=width, fbheight=height;
     /* With Retina display on Mac OS X, GLFW's FramebufferSize
-     is different from WindowSize */
+	   is different from WindowSize */
     glfwGetFramebufferSize(window, &fbwidth, &fbheight);
 
 	GLfloat fov = 90.0f;
@@ -330,51 +333,116 @@ VAO *triangle, *rectangle;
 // Creates the triangle object used in this sample code
 void createTriangle ()
 {
-  /* ONLY vertices between the bounds specified in glm::ortho will be visible on screen */
+	/* ONLY vertices between the bounds specified in glm::ortho will be visible on screen */
 
-  /* Define vertex array as used in glBegin (GL_TRIANGLES) */
-  static const GLfloat vertex_buffer_data [] = {
-    0, 1,0, // vertex 0
-    -1,-1,0, // vertex 1
-    1,-1,0, // vertex 2
-  };
+	/* Define vertex array as used in glBegin (GL_TRIANGLES) */
+	static const GLfloat vertex_buffer_data [] = {
+		0, 1,0, // vertex 0
+		-1,-1,0, // vertex 1
+		1,-1,0, // vertex 2
+	};
 
-  static const GLfloat color_buffer_data [] = {
-    1,0,0, // color 0
-    0,1,0, // color 1
-    0,0,1, // color 2
-  };
+	static const GLfloat color_buffer_data [] = {
+		1,0,0, // color 0
+		0,1,0, // color 1
+		0,0,1, // color 2
+	};
 
-  // create3DObject creates and returns a handle to a VAO that can be used later
-  triangle = create3DObject(GL_TRIANGLES, 3, vertex_buffer_data, color_buffer_data, GL_LINE);
+	// create3DObject creates and returns a handle to a VAO that can be used later
+	triangle = create3DObject(GL_TRIANGLES, 3, vertex_buffer_data, color_buffer_data, GL_LINE);
 }
 
 // Creates the rectangle object used in this sample code
 void createRectangle ()
 {
-  // GL3 accepts only Triangles. Quads are not supported
-  static const GLfloat vertex_buffer_data [] = {
-    -1.2,-1,0, // vertex 1
-    1.2,-1,0, // vertex 2
-    1.2, 1,0, // vertex 3
+	// GL3 accepts only Triangles. Quads are not supported
+	static const GLfloat vertex_buffer_data [] = {
+		-1.2,-1,0, // vertex 1
+		1.2,-1,0, // vertex 2
+		1.2, 1,0, // vertex 3
 
-    1.2, 1,0, // vertex 3
-    -1.2, 1,0, // vertex 4
-    -1.2,-1,0  // vertex 1
-  };
+		1.2, 1,0, // vertex 3
+		-1.2, 1,0, // vertex 4
+		-1.2,-1,0  // vertex 1
+	};
 
-  static const GLfloat color_buffer_data [] = {
-    1,0,0, // color 1
-    0,0,1, // color 2
-    0,1,0, // color 3
+	static const GLfloat color_buffer_data [] = {
+		1,0,0, // color 1
+		0,0,1, // color 2
+		0,1,0, // color 3
 
-    0,1,0, // color 3
-    0.3,0.3,0.3, // color 4
-    1,0,0  // color 1
-  };
+		0,1,0, // color 3
+		0.3,0.3,0.3, // color 4
+		1,0,0  // color 1
+	};
 
-  // create3DObject creates and returns a handle to a VAO that can be used later
-  rectangle = create3DObject(GL_TRIANGLES, 6, vertex_buffer_data, color_buffer_data, GL_FILL);
+	// create3DObject creates and returns a handle to a VAO that can be used later
+	rectangle = create3DObject(GL_TRIANGLES, 6, vertex_buffer_data, color_buffer_data, GL_FILL);
+}
+
+void createFloor ()
+{
+	// GL3 accepts only Triangles. Quads are not supported
+	static const GLfloat vertex_buffer_data [] = {
+		-4,-4,0, // vertex 1
+		4,-4,0, // vertex 2
+		4, -3,0, // vertex 3
+
+		4, -3,0, // vertex 3
+		-4, -3,0, // vertex 4
+		-4,-4,0  // vertex 1
+	};
+
+	static const GLfloat color_buffer_data [] = {
+		1,0,0, // color 1
+		1,0,0, // color 2
+		1,0,0, // color 3
+
+		1,0,0, // color 3
+		1,0,0, // color 4
+		1,0,0  // color 1
+	};
+
+	// create3DObject creates and returns a handle to a VAO that can be used later
+	Floor.sprite = create3DObject(GL_TRIANGLES, 6, vertex_buffer_data, color_buffer_data, GL_FILL);
+}
+
+void createBird ()
+{
+	Bird.x = 0;
+	Bird.y = 0;
+	Bird.vel_x = 0;
+	Bird.vel_y = 0;
+
+	static GLfloat vertex_buffer_data [3*363];
+	static GLfloat color_buffer_data [3*363];
+
+	int k = 0;
+	vertex_buffer_data[0] = 0;
+	color_buffer_data[0] = 1;
+	k++;
+	vertex_buffer_data[1] = 0;
+	color_buffer_data[1] = 1;
+	k++;
+	vertex_buffer_data[2] = 0;
+	color_buffer_data[2] = 1;
+	k++;
+
+	for (int i=1; i<=360; ++i)
+	{
+		vertex_buffer_data[k] = cos((i*M_PI)/180);
+		color_buffer_data[k] = 1;
+		k++;
+		vertex_buffer_data[k] = sin((i*M_PI)/180);
+		color_buffer_data[k] = 0;
+		k++;
+		vertex_buffer_data[k] = 0;
+		color_buffer_data[k] = 0;
+		k++;
+	}
+
+	// create3DObject creates and returns a handle to a VAO that can be used later
+	Bird.sprite = create3DObject(GL_TRIANGLE_FAN, 361, vertex_buffer_data, color_buffer_data, GL_FILL);
 }
 
 float camera_rotation_angle = 90;
@@ -385,70 +453,89 @@ float triangle_rotation = 0;
 /* Edit this function according to your assignment */
 void draw ()
 {
-  // clear the color and depth in the frame buffer
-  glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	// clear the color and depth in the frame buffer
+	glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-  // use the loaded shader program
-  // Don't change unless you know what you are doing
-  glUseProgram (programID);
+	// use the loaded shader program
+	// Don't change unless you know what you are doing
+	glUseProgram (programID);
 
-  // Eye - Location of camera. Don't change unless you are sure!!
-  glm::vec3 eye ( 5*cos(camera_rotation_angle*M_PI/180.0f), 0, 5*sin(camera_rotation_angle*M_PI/180.0f) );
-  // Target - Where is the camera looking at.  Don't change unless you are sure!!
-  glm::vec3 target (0, 0, 0);
-  // Up - Up vector defines tilt of camera.  Don't change unless you are sure!!
-  glm::vec3 up (0, 1, 0);
+	// Eye - Location of camera. Don't change unless you are sure!!
+	glm::vec3 eye ( 5*cos(camera_rotation_angle*M_PI/180.0f), 0, 5*sin(camera_rotation_angle*M_PI/180.0f) );
+	// Target - Where is the camera looking at.  Don't change unless you are sure!!
+	glm::vec3 target (0, 0, 0);
+	// Up - Up vector defines tilt of camera.  Don't change unless you are sure!!
+	glm::vec3 up (0, 1, 0);
 
-  // Compute Camera matrix (view)
-  // Matrices.view = glm::lookAt( eye, target, up ); // Rotating Camera for 3D
-  //  Don't change unless you are sure!!
-  Matrices.view = glm::lookAt(glm::vec3(0,0,3), glm::vec3(0,0,0), glm::vec3(0,1,0)); // Fixed camera for 2D (ortho) in XY plane
+	// Compute Camera matrix (view)
+	// Matrices.view = glm::lookAt( eye, target, up ); // Rotating Camera for 3D
+	//  Don't change unless you are sure!!
+	Matrices.view = glm::lookAt(glm::vec3(0,0,3), glm::vec3(0,0,0), glm::vec3(0,1,0)); // Fixed camera for 2D (ortho) in XY plane
 
-  // Compute ViewProject matrix as view/camera might not be changed for this frame (basic scenario)
-  //  Don't change unless you are sure!!
-  glm::mat4 VP = Matrices.projection * Matrices.view;
+	// Compute ViewProject matrix as view/camera might not be changed for this frame (basic scenario)
+	//  Don't change unless you are sure!!
+	glm::mat4 VP = Matrices.projection * Matrices.view;
 
-  // Send our transformation to the currently bound shader, in the "MVP" uniform
-  // For each model you render, since the MVP will be different (at least the M part)
-  //  Don't change unless you are sure!!
-  glm::mat4 MVP;	// MVP = Projection * View * Model
+	// Send our transformation to the currently bound shader, in the "MVP" uniform
+	// For each model you render, since the MVP will be different (at least the M part)
+	//  Don't change unless you are sure!!
+	glm::mat4 MVP;	// MVP = Projection * View * Model
 
-  // Load identity to model matrix
-  Matrices.model = glm::mat4(1.0f);
+	// Load identity to model matrix
+	Matrices.model = glm::mat4(1.0f);
 
-  /* Render your scene */
+	/* Render your scene */
 
-  glm::mat4 translateTriangle = glm::translate (glm::vec3(-2.0f, 0.0f, 0.0f)); // glTranslatef
-  glm::mat4 rotateTriangle = glm::rotate((float)(triangle_rotation*M_PI/180.0f), glm::vec3(0,0,1));  // rotate about vector (1,0,0)
-  glm::mat4 triangleTransform = translateTriangle * rotateTriangle;
-  Matrices.model *= triangleTransform; 
-  MVP = VP * Matrices.model; // MVP = p * V * M
+	glm::mat4 translateTriangle = glm::translate (glm::vec3(-2.0f, 0.0f, 0.0f)); // glTranslatef
+	glm::mat4 rotateTriangle = glm::rotate((float)(triangle_rotation*M_PI/180.0f), glm::vec3(0,0,1));  // rotate about vector (1,0,0)
+	glm::mat4 triangleTransform = translateTriangle * rotateTriangle;
+	Matrices.model *= triangleTransform;
+	MVP = VP * Matrices.model; // MVP = p * V * M
 
-  //  Don't change unless you are sure!!
-  glUniformMatrix4fv(Matrices.MatrixID, 1, GL_FALSE, &MVP[0][0]);
+	//  Don't change unless you are sure!!
+	glUniformMatrix4fv(Matrices.MatrixID, 1, GL_FALSE, &MVP[0][0]);
 
-  // draw3DObject draws the VAO given to it using current MVP matrix
-  draw3DObject(triangle);
+	// draw3DObject draws the VAO given to it using current MVP matrix
+	// draw3DObject(triangle);
 
-  // Pop matrix to undo transformations till last push matrix instead of recomputing model matrix
-  // glPopMatrix ();
-  Matrices.model = glm::mat4(1.0f);
+	// Pop matrix to undo transformations till last push matrix instead of recomputing model matrix
+	// glPopMatrix ();
+	Matrices.model = glm::mat4(1.0f);
 
-  glm::mat4 translateRectangle = glm::translate (glm::vec3(2, 0, 0));        // glTranslatef
-  glm::mat4 rotateRectangle = glm::rotate((float)(rectangle_rotation*M_PI/180.0f), glm::vec3(0,0,1)); // rotate about vector (-1,1,1)
-  Matrices.model *= (translateRectangle * rotateRectangle);
-  MVP = VP * Matrices.model;
-  glUniformMatrix4fv(Matrices.MatrixID, 1, GL_FALSE, &MVP[0][0]);
+	glm::mat4 translateRectangle = glm::translate (glm::vec3(2, 0, 0));        // glTranslatef
+	glm::mat4 rotateRectangle = glm::rotate((float)(rectangle_rotation*M_PI/180.0f), glm::vec3(0,0,1)); // rotate about vector (-1,1,1)
+	Matrices.model *= (translateRectangle * rotateRectangle);
+	MVP = VP * Matrices.model;
+	glUniformMatrix4fv(Matrices.MatrixID, 1, GL_FALSE, &MVP[0][0]);
 
-  // draw3DObject draws the VAO given to it using current MVP matrix
-  draw3DObject(rectangle);
+	// draw3DObject draws the VAO given to it using current MVP matrix
+	// draw3DObject(rectangle);
 
-  // Increment angles
-  float increments = 1;
+//####################################################################################################
 
-  //camera_rotation_angle++; // Simulating camera rotation
-  triangle_rotation = triangle_rotation + increments*triangle_rot_dir*triangle_rot_status;
-  rectangle_rotation = rectangle_rotation + increments*rectangle_rot_dir*rectangle_rot_status;
+	Matrices.model = glm::mat4(1.0f);
+	MVP = VP * Matrices.model;
+	glUniformMatrix4fv(Matrices.MatrixID, 1, GL_FALSE, &MVP[0][0]);
+	// draw3DObject draws the VAO given to it using current MVP matrix
+	draw3DObject(Floor.sprite);
+
+	Matrices.model = glm::mat4(1.0f);
+	glm::mat4 translateBird = glm::translate (glm::vec3(Bird.x, Bird.y, 0));        // glTranslatef
+	glm::mat4 rotateBird = glm::rotate((float)(rectangle_rotation*M_PI/180.0f), glm::vec3(0,0,1)); // rotate about vector (-1,1,1)
+	Matrices.model *= translateBird;
+	MVP = VP * Matrices.model;
+	glUniformMatrix4fv(Matrices.MatrixID, 1, GL_FALSE, &MVP[0][0]);
+	// draw3DObject draws the VAO given to it using current MVP matrix
+	draw3DObject(Bird.sprite);
+
+//####################################################################################################
+
+	// Increment angles
+	float increments = 1;
+
+	//camera_rotation_angle++; // Simulating camera rotation
+	triangle_rotation = triangle_rotation + increments*triangle_rot_dir*triangle_rot_status;
+	rectangle_rotation = rectangle_rotation + increments*rectangle_rot_dir*rectangle_rot_status;
 }
 
 /* Initialise glfw window, I/O callbacks and the renderer to use */
@@ -459,8 +546,8 @@ GLFWwindow* initGLFW (int width, int height)
 
     glfwSetErrorCallback(error_callback);
     if (!glfwInit()) {
-        exit(EXIT_FAILURE);
-    }
+	exit(EXIT_FAILURE);
+}
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -470,9 +557,9 @@ GLFWwindow* initGLFW (int width, int height)
     window = glfwCreateWindow(width, height, "Sample OpenGL 3.3 Application", NULL, NULL);
 
     if (!window) {
-        glfwTerminate();
-        exit(EXIT_FAILURE);
-    }
+	glfwTerminate();
+	exit(EXIT_FAILURE);
+}
 
     glfwMakeContextCurrent(window);
     gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
@@ -482,7 +569,7 @@ GLFWwindow* initGLFW (int width, int height)
 
     /* Register function to handle window resizes */
     /* With Retina display on Mac OS X GLFW's FramebufferSize
-     is different from WindowSize */
+	   is different from WindowSize */
     glfwSetFramebufferSizeCallback(window, reshapeWindow);
     glfwSetWindowSizeCallback(window, reshapeWindow);
 
@@ -507,17 +594,18 @@ void initGL (GLFWwindow* window, int width, int height)
 	// Create the models
 	createTriangle (); // Generate the VAO, VBOs, vertices data & copy into the array buffer
 	createRectangle ();
-	
+	createFloor ();
+	createBird ();
+
 	// Create and compile our GLSL program from the shaders
 	programID = LoadShaders( "Sample_GL.vert", "Sample_GL.frag" );
 	// Get a handle for our "MVP" uniform
 	Matrices.MatrixID = glGetUniformLocation(programID, "MVP");
-
 	
 	reshapeWindow (window, width, height);
 
     // Background color of the scene
-	glClearColor (0.3f, 0.3f, 0.3f, 0.0f); // R, G, B, A
+	glClearColor (1.0f, 0.0f, 10.0f, 0.0f); // R, G, B, A
 	glClearDepth (1.0f);
 
 	glEnable (GL_DEPTH_TEST);
@@ -531,8 +619,8 @@ void initGL (GLFWwindow* window, int width, int height)
 
 int main (int argc, char** argv)
 {
-	int width = 600;
-	int height = 600;
+	int width = 800;
+	int height = 800;
 
     GLFWwindow* window = initGLFW(width, height);
 
@@ -543,22 +631,22 @@ int main (int argc, char** argv)
     /* Draw in loop */
     while (!glfwWindowShouldClose(window)) {
 
-        // OpenGL Draw commands
-        draw();
+	// OpenGL Draw commands
+	draw();
 
-        // Swap Frame Buffer in double buffering
-        glfwSwapBuffers(window);
+	// Swap Frame Buffer in double buffering
+	glfwSwapBuffers(window);
 
-        // Poll for Keyboard and mouse events
-        glfwPollEvents();
+	// Poll for Keyboard and mouse events
+	glfwPollEvents();
 
-        // Control based on time (Time based transformation like 5 degrees rotation every 0.5s)
-        current_time = glfwGetTime(); // Time in seconds
-        if ((current_time - last_update_time) >= 0.5) { // atleast 0.5s elapsed since last frame
-            // do something every 0.5 seconds ..
-            last_update_time = current_time;
-        }
-    }
+	// Control based on time (Time based transformation like 5 degrees rotation every 0.5s)
+	current_time = glfwGetTime(); // Time in seconds
+	if ((current_time - last_update_time) >= 0.5) { // atleast 0.5s elapsed since last frame
+	// do something every 0.5 seconds ..
+	last_update_time = current_time;
+}
+}
 
     glfwTerminate();
     exit(EXIT_SUCCESS);
